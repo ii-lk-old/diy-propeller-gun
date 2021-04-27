47766d96d415a0ea38dc33c11c747511f3443919
Dileepa Jayawardena <me.dreamwalker@gmail.com>
2021-04-27 11:10:12 +0530

    Code imported from speed test

--- bl_propeller_gun/bl_propeller_gun.ino
+++ bl_propeller_gun/bl_propeller_gun.ino
@@ -1,7 +1,25 @@
-void setup(){
+#include <Servo.h>
+#define ESC_PIN 3
 
+Servo esc;
+
+void setup() {
+  Serial.begin(115200);
+  esc.attach(ESC_PIN,1000,3000);
+  Serial.println("esc attached");
+
+  esc.write(0);
+  delay(3000);
+  Serial.println("esc ready");
+
+  esc.write(180);
+  Serial.println("reaching to max speed");
+  delay(3000);
+  
+  esc.write(0);
+  Serial.println("motor stopped");
 }
 
-void loop(){
-
+void loop() {
+  
 }

0fc2b61642350ab21ab72928efb477b10f719d5b
Dileepa Jayawardena <me.dreamwalker@gmail.com>
2021-04-27 11:52:21 +0530

    button denounced and relay inverted

--- bl_propeller_gun/bl_propeller_gun.ino
+++ bl_propeller_gun/bl_propeller_gun.ino
@@ -11,10 +11,11 @@
 #define MODE_ARM 1
 #define MODE_MAX_SPEED 2
 
-#define relay_active false
-
 int mode;
+
+#define relay_active true
 Servo esc;
+
 
 void setup() {
   Serial.begin(115200);
@@ -22,14 +23,17 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT); // relay init
   digitalWrite(RELAY_PIN, !relay_active); // off relay
 
+
   esc.attach(3, 1000, 2000);//motor init
   Serial.println("esc attached");
   mode = MODE_DISARM;
 
+  delay(500);
+  pinMode(PIN_BUZZER, OUTPUT);
   tone(PIN_BUZZER, 3000, 50);
   delay(500);
 }
-
+int release_count = 0;
 void loop() {
   if (mode == MODE_DISARM) {
     arm_esc();
@@ -41,8 +45,16 @@ void loop() {
     if (digitalRead(PIN_BT2)) {
       motor_break();
     } else if (!digitalRead(PIN_BT1)) {
+      release_count++;
+      if (release_count > 5) {
         slow_down();
+        return;
+      }
+    } else {
+      release_count = 0;
+    }
   }
+  delay(20);
 }
 
 void arm_esc() {
@@ -54,10 +66,24 @@ void arm_esc() {
 }
 
 void max_speed() {
+  release_count = 0;
   mode = MODE_MAX_SPEED;
-  esc.write(i);
-  delay(4000);//speed up delay
-  tone(PIN_BUZZER, 6000, 1000);
+  for (int i = 50; i <= 180; i++) {
+    esc.write(i);
+    delay(20);
+    if (!digitalRead(PIN_BT1)) {
+      release_count++;
+      if (release_count > 5) {
+        slow_down();
+        return;
+      }
+    } else {
+      release_count = 0;
+    }
+
+  }
+  tone(PIN_BUZZER, 6000, 500);
+
   Serial.print("max speed");
   Serial.println("-done");
 }
@@ -65,8 +91,6 @@ void max_speed() {
 void motor_break() {
   esc.write(0);
   Serial.print("motor stopped");
-
-  //motor breaking 
   digitalWrite(RELAY_PIN, relay_active); // on relay
   Serial.print("relay on");
   tone(PIN_BUZZER, 6000, 100);
@@ -80,10 +104,9 @@ void slow_down() {
   esc.write(0);
   Serial.print("motor stopped");
   mode = MODE_ARM;
-   _delay(2000, -1);
+  _delay(2000, -1);
   Serial.println("-done");
 }
-
 
 void _delay(int duration, int tone_dir) {
   long ltime = millis();

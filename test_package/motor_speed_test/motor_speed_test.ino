aa94e81b7f52d672239db07aaacc332d570677a0
Dileepa Jayawardena <me.dreamwalker@gmail.com>
2021-04-27 10:54:12 +0530

    Debugging serial print

--- test_package/motor_speed_test/motor_speed_test.ino
+++ test_package/motor_speed_test/motor_speed_test.ino
@@ -6,12 +6,14 @@ Servo esc;
 void setup() {
   Serial.begin(115200);//baudrate 115200
   esc.attach(ESC_PIN,1000,3000); 
+  Serial.println("esc attached");
 
   esc.write(0);
   delay(3000);//arming delay 
+  Serial.println("esc ready");
 
   esc.write(180);
-  delay(3000); 
+  Serial.println("reaching to max speed"); 
 }
 
 void loop() {
